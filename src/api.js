5d2b7ac4c004c8372689e8eff5f89a044979cc2a
Daniel Nelson <daniel@populr.me>
2014-11-06 09:36:29 -0600

    Include api.Element

--- src/api.js
+++ src/api.js
@@ -2,12 +2,14 @@ define([
   './api/command-patch',
   './api/command',
   './api/node',
+  './api/element',
   './api/selection',
   './api/simple-command'
 ], function (
   buildCommandPatch,
   buildCommand,
   Node,
+  Element,
   buildSelection,
   buildSimpleCommand
 ) {
@@ -18,6 +20,7 @@ define([
     this.CommandPatch = buildCommandPatch(scribe);
     this.Command = buildCommand(scribe);
     this.Node = Node;
+    this.Element = Element;
     this.Selection = buildSelection(scribe);
     this.SimpleCommand = buildSimpleCommand(this, scribe);
   };
